df16b23faf811ba892c48dde2f6a98e946304e14
atasen19 <atasen19@itu.edu.tr>
2023-03-30 23:44:13 +0300

    feat: add comment line for db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017/adcase')],
+  // I could't set up the infra properly. The link above should change with the DocumentDB ARN/URL.
   controllers: [AppController],
   providers: [AppService],
 })
